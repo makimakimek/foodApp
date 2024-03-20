181ad71819c6015ef5e296f649aaa8415b099a19
makimakimek <melodiezgi17@gmail.com>
2024-03-21 02:10:55 +0300

    landing page's header buttons + header title + underneath buttons all added with css

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,53 @@
+import React, { useState } from 'react';
 import './App.css'
 
 function App() {
 
   return (
     <>
-      <div className = "header">
+      <div className = "page">
+        <div className = "header">
+          <div className = "leftButtons">
+            <button className = "appNameButton">DishDash</button>
+          </div>
+
+          <div className = "middleButtons">
+            <button className = "companyButton">Company</button>
+            <button className = "faqButton">FAQs</button>
+            <button className = "blogButton">Blog</button>
+            <button className = "contactButton">Contact</button>
+          </div>
+
+          <div className = "rightButtons">
+            <button className = "customersButton">Customers</button>
+          </div>
+        </div>
+
+        <div className = "landingPageSloganAndButtons">
+          <div className = "landingPageSlogan">
+            Have you eaten?
+          </div>
+          
+          <div className = "appStoreButtons">
+            <button className = "googlePlayButton">Download on Google Play</button>
+            <button className = "appStoreButton">Download on App Store</button>
+          </div>
+        </div>
+
+        <div className = "threeToggle">
+          <div class="switch">
+            <input id="switch-y" name="tripple" type="radio" value="Y" class="switch-input" checked/>
+            <label for="switch-y" class="switch-label switch-label-y">Customer</label>
         
+            <input id="switch-i" name="tripple" type="radio" value="I" class="switch-input" />
+            <label for="switch-i" class="switch-label switch-label-i">Vendors</label>
+        
+            <input id="switch-n" name="tripple" type="radio" value="N" class="switch-input" />
+            <label for="switch-n" class="switch-label switch-label-n">Riders</label>
+        
+            <span class="switch-selector"></span>
+          </div>
+        </div>
       </div>
     </>
   )
